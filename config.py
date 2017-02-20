821a0f88e7ab83f98bbcdfbf1d71404ca9bf17cc
mac <mac@macui-MacBook-Pro.local>
2017-02-20 22:43:51 +0900

    config fix

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
 SECRET_KEY = os.environ['SECRET_KEY']
-MYSQL_DB_NAME = os.environ['MYSQL_DB_NAME']
-MYSQL_DB_ROOT = os.environ['MYSQL_DB_ROOT']
-MYSQL_DB_PS = os.environ['MYSQL_DB_PS']
+DB_NAME = os.environ['DB_NAME']
+DB_ID = os.environ['DB_ID']
+DB_PS = os.environ['DB_PS']
+DB_HOST = os.environ['DB_HOST']
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{0]:{1}@{2}/{3}".format(DB_ID,
+                                                                   DB_PS,
+                                                                   DB_HOST,
+                                                                   DB_NAME)
